69014550d47ad573a338f0b5c9b41bb0757147ae
czyjtu <maciejczyjtu@gmail.com>
2024-08-26 23:17:55 +0200

    features grid umap

--- generate_dataset.sh
+++ generate_dataset.sh
@@ -1,2 +1,3 @@
 #!bin/bash
-python scripts/generate_embeddings.py --dataset_dir "/Users/czyjtu/dev/prompt2avatar/experiments/medium_geneset_embeddings/predicted_faces_mlp/predicted_dnas_mlp" --out "/Users/czyjtu/dev/prompt2avatar/experiments/medium_geneset_embeddings/predicted_faces_mlp/predicted_dnas_mlp_embeddings" --model_type "insightface" --weights "./thirdparty/recognition/weights/arcface_r100.pth"
+python scripts/generate_embeddings.py --dataset_dir "/Users/czyjtu/dev/prompt2avatar/data/genes_grid/gene_forehead_brow_height.forehead_brow_height_pos_skin_color[1]" \
+--out "/Users/czyjtu/dev/prompt2avatar/data/genes_grid/gene_forehead_brow_height.forehead_brow_height_pos_skin_color[1]_embeddings" --model_type "insightface" --weights "./thirdparty/recognition/weights/arcface_r100.pth"
